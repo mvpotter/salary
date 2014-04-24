f8a3458fe884ea41a11c23d6cbff2461b1bca5fa
mvpotter <supermegapotter@gmail.com>
2014-04-24 17:52:38 +0700

    Hour rate is added

--- salary/js/salary.js
+++ salary/js/salary.js
@@ -2,26 +2,30 @@ var app = angular.module('salary', []);
 
 app.controller('SalaryCtrl', function($scope) {
 
+	var WORK_HOURS_IN_A_MONTH = 200;
 	var MONTHS_IN_A_QUARTER = 3;
 	var MONTHS_IN_A_YEAR = 12;
 	var QUARTERS_IN_A_YEAR = 4;
 
+	$scope.update_hour_rate = function() {
+		$scope.monthly = $scope.hourRate * WORK_HOURS_IN_A_MONTH; 
+		$scope.update_monthly_salary(); 
+	};
+
 	$scope.update_monthly_salary = function() {
-		var monthly = $scope.monthly;
-		$scope.quarter = monthly * MONTHS_IN_A_QUARTER;
-		$scope.annual = monthly * MONTHS_IN_A_YEAR; 
+		$scope.quarter = $scope.monthly * MONTHS_IN_A_QUARTER;
+		$scope.update_quarter_salary();
 	};
 
 	$scope.update_quarter_salary = function() {
-		var quarter = $scope.quarter;
-		$scope.monthly = quarter / MONTHS_IN_A_QUARTER;
-		$scope.annual = quarter * QUARTERS_IN_A_YEAR; 
+		$scope.annual = $scope.quarter * QUARTERS_IN_A_YEAR;
+		$scope.update_annual_salary();
 	};
 
 	$scope.update_annual_salary = function() {
-		var annual = $scope.annual;
-		$scope.monthly = annual / MONTHS_IN_A_YEAR;
-		$scope.quarter = annual / QUARTERS_IN_A_YEAR;
+		$scope.quarter = $scope.annual / QUARTERS_IN_A_YEAR;
+		$scope.monthly = $scope.quarter / MONTHS_IN_A_QUARTER;
+		$scope.hourRate = $scope.monthly / WORK_HOURS_IN_A_MONTH;
 	};
 
 });
